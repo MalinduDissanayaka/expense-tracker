4cc7000c4473588ab9529c52a4c35af4a954f0b8
malindu <malindul533@gmail.com>
2026-06-12 20:46:35 +0530

    upadate

--- app/analytics/page.tsx
+++ app/analytics/page.tsx
@@ -67,7 +67,7 @@ export default function Analytics() {
       }
     });
 
-    // Format data for Recharts component
+    // Format data for Recharts componentts
     return Object.keys(categoryTotals)
       .map((key) => ({
         name: key,
